d14e805b6b7c5ab24aaa63de9c9a28b11894d0a3
Zahar-Ya <zahar4877@gmail.com>
2022-10-31 23:00:13 +0300

    add some code

--- First-file.py
+++ First-file.py
@@ -1,2 +1,20 @@
 print('Hello World')
 print('Отлично получается')
+# чтобы прокоментировать сразу много: Ctrl + /
+# nm = [] - список(массив)
+# nm.append(элемент который добавить)
+# nm.insert(№ позиции, что вставить)
+# nm.extend(вставка массива)
+# .sort()
+# .reverse()
+# .pop(индекс удаляемого эл)
+# .remove(какой эл удалить, не индекс, а чему равен)
+# .clear() 
+# .count(какой эл)
+l = int(input('lengh '))
+i = 0
+user = []
+while i<l:
+    i+=1
+    user.append(input('введите элемент '))
+print(user)
